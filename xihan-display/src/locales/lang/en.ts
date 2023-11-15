59df0384d883e9ca3e5b0c68b7bfbe16a0f905c4
zhaifanhua <zhaifanhua@foxmail.com>
2023-11-16 04:54:46 +0800

    init

---
+++ xihan-display/src/locales/lang/en.ts
@@ -0,0 +1,20 @@
+const locale: I18nType.Schema = {
+  system: {
+    title: 'XiHan'
+  },
+  common: {
+    add: 'Add',
+    addSuccess: 'Add Success',
+    edit: 'Edit',
+    editSuccess: 'Edit Success',
+    delete: 'Delete',
+    deleteSuccess: 'Delete Success',
+    batchDelete: 'Batch Delete',
+    confirm: 'Confirm',
+    cancel: 'Cancel',
+    pleaseCheckValue: 'Please check the value is valid',
+    action: 'Action'
+  }
+};
+
+export default locale;
